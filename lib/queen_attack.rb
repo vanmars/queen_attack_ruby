529bdbb0a4cf904213d0d3458eeb4021ae8736dd
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-21 14:54:39 -0700

    Write test for first spec and fail test

---
+++ lib/queen_attack.rb
@@ -0,0 +1,5 @@
+class Array
+  def queen_attack(array)
+    
+  end
+end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -1,2 +1,8 @@
 require ('rspec')
 require ('queen_attack')
+
+describe('Array#queen_attack') do
+  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
+    expect([0,0].queen_attack?([1, 2])).to(eq(false))
+  end
+end
