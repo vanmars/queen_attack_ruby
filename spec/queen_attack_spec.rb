c101ada41cdf1b7e4caeb78da632ab9ab8699f26
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-21 15:02:12 -0700

    Write test 3 and fail

--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -9,4 +9,8 @@ describe('Array#queen_attack') do
   it('is true if the coordinates are horizontally in line with each other') do
     expect([0,0].queen_attack?([3, 0])).to(eq(true))
   end
+
+  it('is true if the coordinates are vertically in line with each other') do
+    expect([0,0].queen_attack?([0, 3])).to(eq(true))
+  end
 end
